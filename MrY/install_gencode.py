a4cac92ea0c30ef82f21b05cfebe0c71379d0700
yy1533 <yun.yan@nyumc.org>
2017-08-24 17:44:16 -0400

    :feet:

---
+++ MrY/install_gencode.py
@@ -0,0 +1,34 @@
+from MrY import get_workflow_fpath
+from snakemake import snakemake
+
+def main(specie=None, assembly=None, release=None, savetodir=None):
+
+    snakefile = get_workflow_fpath(fname='gencode.snakemake')
+    success = snakemake(
+        snakefile=snakefile,
+        targets=args.target,
+
+        configfile=args.config_file,
+        config={'output_dir': args.output_dir,
+                'experiment_table': args.experiment_table},
+
+        printshellcmds=True,
+        printreason=True,
+        timestamp=True,
+        latency_wait=1800,
+        jobname="celseq2_job.{rulename}.{jobid}.sh",
+
+        dryrun=args.dryrun,
+        lock=not args.nolock,
+        unlock=args.unlock,
+
+        cluster=args.cluster,
+        cores=args.cores,
+        nodes=args.cores,
+
+        force_incomplete=args.rerun_incomplete,
+        ignore_incomplete=args.ignore_incomplete)
+
+    sys.exit(0 if success else 1)
+
+    pass
