1ee16740a3fd0d4f932f6b3ec804a6581cb46162
yy1533 <yun.yan@nyumc.org>
2017-08-30 10:41:40 -0400

    :feet: LIST

--- MrY/list.py
+++ MrY/list.py
@@ -5,6 +5,7 @@ import os
 import yaml
 import glob
 from tabulate import tabulate
+from collections import defaultdict, OrderedDict
 from MrY import join_path as jpath
 from MrY import base_name, dir_name
 from MrY import print_logger
@@ -21,7 +22,30 @@ human GRCh38 90 Yes Yes No No Yes No No
 '''
 
 
+def tag_installed(filelist):
+    tag = OrderedDict({'Genome(.fa)': False, 'Genome(.fa.gz)': False,
+                       'GTF(.gtf.gz)': False, 'GFF3(.gff3.gz)': False,
+                       'Bowtie2': False, 'STAR': False})
+    for f in filelist:
+        if f.endswith('.fa'):
+            tag['Genome(.fa)'] = True
+        elif f.endswith('.fa.gz'):
+            tag['Genome(.fa.gz)'] = True
+        elif f.endswith('.gtf.gz'):
+            tag['GTF(.gtf.gz)'] = True
+        elif f.endswith('.gff3.gz'):
+            tag['GFF3(.gff3.gz)'] = True
+        elif f.endswith('.bt2'):
+            tag['Bowtie2'] = True
+        elif f.endswith('.star'):  # to-do
+            tag['STAR'] = True
+        else:
+            continue
+    return(tag)
+
+
 def list_avail(args):
+
     with open(get_species_name_fpath(fname='species_name.yaml'), 'rt') as fin:
         preset = yaml.load(fin)
         SPECIES_SUPPORTED = preset.get('common_scientific_name_pairs',
@@ -35,34 +59,79 @@ def list_avail(args):
 
     ROOTDIR = args.get('root_dir', '.')
 
-    # rootdir/type/org/assembly/release/file => [org, assembly, release, file]
-    genome_fagz_avail = map(lambda fpath: fpath
-                            .replace(jpath(ROOTDIR, 'genome', os.sep), '')
-                            .split(os.sep),
-                            glob.iglob(jpath(ROOTDIR, 'genome',
-                                             '*', '*', '*',
-                                             '*.fa.gz')))
-    genome_fa_avail = map(lambda x: base_name(x, ext='gz'),
-                          genome_fagz_avail)
+    savetofile = args.get('snapshot', None)
+
+    # [rootdir/species/type/org/assembly/release/file] =>
+    # [[species, org, assembly, release, file]]
+    genome_fa_avail = map(lambda fpath: fpath
+                          .replace(jpath(ROOTDIR, os.sep), '')
+                          .replace(jpath('genome', os.sep), '')
+                          .split(os.sep),
+                          glob.iglob(jpath(ROOTDIR, '*',
+                                           'genome',
+                                           '*', '*', '*',
+                                           '*.fa')))
+    genome_fagz_avail = map(lambda x: [x[:len(x) - 1], x[-1] + '.gz'],
+                            genome_fa_avail)
 
     gtf_avail = map(lambda fpath: fpath
-                    .replace(jpath(ROOTDIR, 'annotation', os.sep), '')
+                    .replace(jpath(ROOTDIR, os.sep), '')
+                    .replace(jpath('annotation', os.sep), '')
                     .split(os.sep),
-                    glob.iglob(jpath(ROOTDIR, 'annotation',
+                    glob.iglob(jpath(ROOTDIR, '*',
+                                     'annotation',
                                      '*', '*', '*',
                                      '*.gtf.gz')))
-    gff_avail = map(lambda fpath: fpath
-                    .replace(jpath(ROOTDIR, 'annotation', os.sep), '')
-                    .split(os.sep),
-                    glob.iglob(jpath(ROOTDIR, 'annotation',
-                                     '*', '*', '*',
-                                     '*.gff3.gz')))
+    gff3_avail = map(lambda fpath: fpath
+                     .replace(jpath(ROOTDIR, os.sep), '')
+                     .replace(jpath('annotation', os.sep), '')
+                     .split(os.sep),
+                     glob.iglob(jpath(ROOTDIR, '*',
+                                      'annotation',
+                                      '*', '*', '*',
+                                      '*.gff3.gz')))
     # 1.bt2, .2.bt2, .3.bt2, .4.bt2, .rev.1.bt2, .rev.2.bt2
     bowtie_avail = map(lambda fpath: fpath
-                       .replace(jpath(ROOTDIR, 'aligner_index', 'bowtie2',
-                                      os.sep), '')
+                       .replace(jpath(ROOTDIR, os.sep), '')
+                       .replace(jpath('aligner_index', 'bowtie2', os.sep), '')
                        .split(os.sep),
-                       glob.iglob(jpath(ROOTDIR, 'aligner_index', 'bowtie2',
+                       glob.iglob(jpath(ROOTDIR, '*',
+                                        'aligner_index', 'bowtie2',
                                         '*', '*', '*',
-                                        '*.gff3.gz')))
+                                        '*.rev.2.bt2')))
+    # to-do
     star_avail = []
+
+    species_avail = genome_fa_avail[:1][:1] + genome_fagz_avail[:1][:1] + \
+        gtf_avail[:1][:1] + gff3_avail[:1][:1] + \
+        bowtie_avail[:1][:1] + star_avail[:1][:1]
+
+    if not species_avail:
+        return(None)
+
+    table = []
+    species_avail_info = dict()
+    species_avail_subinfo = defaultdict(list)
+    for info_avail in [genome_fa_avail, genome_fagz_avail,
+                       gtf_avail, gff3_avail,
+                       bowtie_avail, star_avail]:
+        for info in info_avail:
+            sp, sp_org_asb_rel, fname = info[0], info[:len(info) - 1], info[-1]
+            sp_org_asb_rel = '/'.join(sp_org_asb_rel)
+            species_avail_info[sp] = sp_org_asb_rel
+            species_avail_subinfo[sp_org_asb_rel].append(fname)
+
+    for sp_org_asb_rel, fl in species_avail_subinfo.items():
+        sp_org_asb_rel_tag = tag_installed(fl)
+        species_avail_subinfo[sp_org_asb_rel] = sp_org_asb_rel_tag
+        table.append(sp_org_asb_rel.split('/') +
+                     sp_org_asb_rel_tag.values())
+
+    table_header = ['Species', 'Org', 'Assembly', 'Release'] + \
+        sp_org_asb_rel_tag.keys()
+
+    table = tabulate(table, headers=table_header)
+    with open(savetofile, 'w') as fout:
+        fout.write(table)
+
+    return(table)
