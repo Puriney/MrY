f2ed195ea8d55b3713ad27b633ad72b71786bf1b
Puriney <youryanyun@gmail.com>
2017-08-31 15:22:54 -0400

    :tada: Release LIST subcommand
    :feet: release searching STAR for LIST subcommand
    
    :feet: release searching STAR for LIST subcommand
    
    :dog: LIST
    
    :tada: LIST contains search function

--- MrY/list_avail.py
+++ MrY/list_avail.py
@@ -2,15 +2,11 @@
 List available installations at root_dir
 '''
 import os
-import yaml
 import glob
 from tabulate import tabulate
 from collections import defaultdict, OrderedDict
-from itertools import islice
+from operator import itemgetter
 from MrY import join_path as jpath
-from MrY import base_name, dir_name
-from MrY import print_logger
-from MrY import get_species_name_fpath
 
 
 '''
@@ -24,21 +20,22 @@ human GRCh38 90 Yes Yes No No Yes No No
 
 
 def tag_installed(filelist):
-    tag = OrderedDict({'Genome(.fa)': False, 'Genome(.fa.gz)': False,
-                       'GTF(.gtf.gz)': False, 'GFF3(.gff3.gz)': False,
+    tag = OrderedDict({'Genome (.fa)': False,
+                       # 'Genome(.fa.gz)': True,
+                       'Annotation (.gtf)': False, 'Annotation (.gff3)': False,
                        'Bowtie2': False, 'STAR': False})
     for f in filelist:
         if f.endswith('.fa'):
-            tag['Genome(.fa)'] = True
-        elif f.endswith('.fa.gz'):
-            tag['Genome(.fa.gz)'] = True
-        elif f.endswith('.gtf.gz'):
-            tag['GTF(.gtf.gz)'] = True
-        elif f.endswith('.gff3.gz'):
-            tag['GFF3(.gff3.gz)'] = True
+            tag['Genome (.fa)'] = True
+        # elif f.endswith('.fa.gz'):
+        #     tag['Genome(.fa.gz)'] = True
+        elif f.endswith('.gtf'):
+            tag['Annotation (.gtf)'] = True
+        elif f.endswith('.gff3'):
+            tag['Annotation (.gff3)'] = True
         elif f.endswith('.bt2'):
             tag['Bowtie2'] = True
-        elif f.endswith('.star'):  # to-do
+        elif f.endswith('SA'):
             tag['STAR'] = True
         else:
             continue
@@ -47,15 +44,9 @@ def tag_installed(filelist):
 
 def list_avail(args):
 
-    with open(get_species_name_fpath(fname='species_name.yaml'), 'rt') as fin:
-        preset = yaml.load(fin)
-        SPECIES_SUPPORTED = preset.get('common_scientific_name_pairs',
-                                       dict()).keys()
-        ORG_SUPPORTED = preset.get('supported_orgs', list())
-
     args = vars(args)
-    species = args.get('species', SPECIES_SUPPORTED)
-    org = args.get('org', ORG_SUPPORTED)
+    species = args.get('species', [])
+    org = args.get('org', [])
     assembly = args.get('assembly', [])
     release = args.get('release', [])
 
@@ -63,8 +54,8 @@ def list_avail(args):
 
     savetofile = args.get('snapshot', None)
 
-    # [rootdir/species/type/org/assembly/release/file] =>
-    # [[species, org, assembly, release, file]]
+    # ['rootdir/species/type/org/assembly/release/file'] =>
+    # [['species', 'org', 'assembly', 'release', 'file']]
     genome_fa_avail = map(lambda fpath: fpath
                           .replace(ROOTDIR + os.sep, '')
                           .replace('genome' + os.sep, '')
@@ -75,14 +66,14 @@ def list_avail(args):
                                                 '*.fa')))
     # genome_fagz_avail = map(lambda x: x,
     #                         genome_fa_avail)
-    genome_fagz_avail = map(lambda fpath: fpath
-                            .replace(ROOTDIR + os.sep, '')
-                            .replace('genome' + os.sep, '')
-                            .split(os.sep),
-                            glob.iglob(jpath(ROOTDIR, '*',
-                                             'genome',
-                                             '*', '*', '*',
-                                             '*.fa.gz')))
+    # genome_fagz_avail = map(lambda fpath: fpath
+    #                         .replace(ROOTDIR + os.sep, '')
+    #                         .replace('genome' + os.sep, '')
+    #                         .split(os.sep),
+    #                         glob.iglob(jpath(ROOTDIR, '*',
+    #                                          'genome',
+    #                                          '*', '*', '*',
+    #                                          '*.fa.gz')))
     gtf_avail = map(lambda fpath: fpath
                     .replace(ROOTDIR + os.sep, '')
                     .replace('annotation' + os.sep, '')
@@ -90,7 +81,7 @@ def list_avail(args):
                     glob.iglob(jpath(ROOTDIR, '*',
                                      'annotation',
                                      '*', '*', '*',
-                                          '*.gtf.gz')))
+                                          '*.gtf')))
     gff3_avail = map(lambda fpath: fpath
                      .replace(ROOTDIR + os.sep, '')
                      .replace('annotation' + os.sep, '')
@@ -98,7 +89,7 @@ def list_avail(args):
                      glob.iglob(jpath(ROOTDIR, '*',
                                       'annotation',
                                       '*', '*', '*',
-                                      '*.gff3.gz')))
+                                      '*.gff3')))
     # 1.bt2, .2.bt2, .3.bt2, .4.bt2, .rev.1.bt2, .rev.2.bt2
     bowtie_avail = map(lambda fpath: fpath
                        .replace(ROOTDIR + os.sep, '')
@@ -108,38 +99,63 @@ def list_avail(args):
                                         'aligner_index', 'bowtie2',
                                         '*', '*', '*',
                                         '*.rev.2.bt2')))
-    # to-do
-    star_avail = []
-
+    # SA, SAindex, ...
+    star_avail = map(lambda fpath: fpath
+                     .replace(ROOTDIR + os.sep, '')
+                     .replace(jpath('aligner_index', 'star') + os.sep, '')
+                     .split(os.sep),
+                     glob.iglob(jpath(ROOTDIR, '*',
+                                      'aligner_index', 'star',
+                                      '*', '*', '*',
+                                      'SA')))
     table = []
-    species_avail_info = dict()
     species_avail_subinfo = defaultdict(list)
-    for info_avail in [genome_fa_avail, genome_fagz_avail,
+    for info_avail in [genome_fa_avail,
+                       # genome_fagz_avail,
                        gtf_avail, gff3_avail,
                        bowtie_avail, star_avail]:
         for info in info_avail:
-            sp, fname = info[0], info[-1]
-            sp_org_asb_rel = info[: len(info) - 1]
-            sp_org_asb_rel = '/'.join(sp_org_asb_rel)
-            species_avail_info[sp] = sp_org_asb_rel
+            sp_org_asb_rel, fname = info[: len(info) - 1], info[-1]
+            sp_org_asb_rel[-1] = sp_org_asb_rel[-1].replace('release_', '')
+            sp_org_asb_rel = (os.sep).join(sp_org_asb_rel)
             species_avail_subinfo[sp_org_asb_rel].append(fname)
 
     for sp_org_asb_rel, fl in species_avail_subinfo.items():
         sp_org_asb_rel_tag = tag_installed(fl)
         species_avail_subinfo[sp_org_asb_rel] = sp_org_asb_rel_tag
-        table.append(sp_org_asb_rel.split('/') +
+        sp_org_asb_rel = sp_org_asb_rel.split(os.sep)
+        sp_org_asb_rel[1], sp_org_asb_rel[2] = \
+            sp_org_asb_rel[2], sp_org_asb_rel[1]
+        sp_asb_org_rel = sp_org_asb_rel
+        table.append(sp_asb_org_rel +
                      list(sp_org_asb_rel_tag.values()))
 
-    table_header = ['Species', 'Org', 'Assembly', 'Release',
-                    'Genome(.fa)', 'Genome(.fa.gz)',
-                    'GTF(.gtf.gz)', 'GFF3(.gff3.gz)',
+    table_header = ['Species', 'Assembly', 'Org', 'Release',
+                    'Genome (.fa)',
+                    # 'Genome (.fa.gz)',
+                    'Annotation (.gtf)', 'Annotation (.gff3)',
                     'Bowtie2', 'STAR']
 
+    table.sort(key=itemgetter(0, 1, 2, 3),
+               reverse=True)  # sort by sp-assembly-org-release
     table = tabulate(table, headers=table_header, tablefmt="rst")
-    # table = tabulate(table)
 
     if savetofile:
         with open(savetofile, 'w') as fout:
             fout.write(table)
             fout.write('\n')
+
+    print('=' * 10)
+    if not species or not assembly or not org or not release:
+        print('Specify species + assembly + org + release, if needed.')
+    else:
+        user_query = (os.sep).join([species[0], org[0],
+                                    assembly[0], release[0]])
+
+        print('{}-{}-{}-{} was queried:'.format(species[0], assembly[0],
+                                                org[0], release[0]))
+        for k, v in species_avail_subinfo.get(user_query, dict()).items():
+            print('\t{:>20}: {}'.format(k,
+                                        'Installed' if v else 'Not-installed'))
+
     return(table)
