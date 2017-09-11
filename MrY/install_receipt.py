5ea61c86d84f4503a8caa12f6bb6273e47c472cf
yy1533 <yun.yan@nyumc.org>
2017-09-11 16:03:48 -0400

    :beer: template for receipt

--- MrY/install_receipt.py
+++ MrY/install_receipt.py
@@ -1,26 +1,47 @@
 import yaml
+from collections import defaultdict
+from MrY import print_logger as printl
 
 
-def new_installation_receipt(fpath):
+def new_installation_receipt(fpath, num=1):
+    '''
+    [{'LINK_FASTA': '',
+    'LINK_GFF3': '',
+    'LINK_GTF': '',
+    'assembly': '',
+    'org': '',
+    'release': '',
+    'species': ''}]
+  '''
     fout = open(fpath, 'w')
-    fout.write('{}: {}'.format('root_dir', '.'))
-
-    # receipt_dict = {
-    #     'species': 'Salmonella_SL1344',
-    #     'assembly': 'ASM21085v2',
-    #     'release': 'GCF_000210855.2',
-    #     'org': 'NCBI',
-    #     'LINK_FASTA': 'path/to/file',
-    #     'LINK_GFF3': 'path/to/file',
-    #     'LINK_GTF': 'path/to/file'
-    # }
-    # with open(fpath, 'w') as fout:
-    #     yaml.dump(receipt_dict, fout)
+    for i in range(num):
+        fout.write('- species: \'{}\'\n'.format(''))
+        fout.write('  assembly: \'{}\'\n'.format(''))
+        fout.write('  release: \'{}\'\n'.format(''))
+        fout.write('  org: \'{}\'\n'.format(''))
+        fout.write('  LINK_FASTA: \'{}\'\n'.format(''))
+        fout.write('  LINK_GFF3: \'{}\'\n'.format(''))
+        fout.write('  LINK_GTF: \'{}\'\n'.format(''))
+    fout.close()
 
 
 def load_installation_receipt(fpath):
+    res = defaultdict(list)
     with open(fpath, 'r') as fin:
-        out = yaml.load(fin)
-    assert set(['LINK_FASTA', 'LINK_GFF3', 'LINK_GTF']) == set(out.keys())
-    return out
-
+        receipts = yaml.load(fin)
+    i, j = 0, 0
+    for receipt in receipts:
+        i += 1
+        if not set(receipt.values()) or set(receipt.values()) == set(['']):
+            printl('Input item-{} is empty.'.format(i))
+            continue
+        j += 1
+        res['species'].append(receipt['species'])
+        res['assembly'].append(receipt['assembly'])
+        res['release'].append(receipt['release'])
+        res['org'].append(receipt['org'])
+        res['LINK_FASTA'].append(receipt['LINK_FASTA'])
+        res['LINK_GFF3'].append(receipt['LINK_GFF3'])
+        res['LINK_GTF'].append(receipt['LINK_GTF'])
+    printl('{}/{} genomes are to be installed.'.format(j, i))
+    return res
